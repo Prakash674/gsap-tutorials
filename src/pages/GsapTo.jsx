00202026d99d4242062cf40a4e76ae0dd947abf5
Prakash Kumar <singhprakash9572@gmail.com>
2024-08-29 09:45:04 +0530

    gsap-Complete

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
-
+  // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: "bounce.in",
+    });
+  });
   return (
     <main>
       <h1>GsapTo</h1>
